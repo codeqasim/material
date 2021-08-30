328f27873b102904b4944f7abb819a764c5c4713
Qasim Hussain <44278286+qaxim@users.noreply.github.com>
2021-08-30 16:32:21 +0500

    init

--- old/home.php
+++
@@ -1,5 +0,0 @@
-<?php $page_name = "Material CSS KIT"; include "_header.php"; ?>
-
-<h2>Home</h2>
-
-<?php include "_footer.php"; ?>
--- old/index.php
+++
@@ -1,6 +0,0 @@
-<?php $page_name = "Material CSS KIT"; include "_header.php"; ?>
-
-<h2>Body</h2>
-
-
-<?php include "_footer.php"; ?>
