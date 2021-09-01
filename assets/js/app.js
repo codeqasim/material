ba6a7528faf457de433f489eca2993935fe1538a
Qasim Hussain <44278286+qaxim@users.noreply.github.com>
2021-09-01 09:05:01 +0500

    i

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,8 +8,8 @@ when('/', {
 templateUrl: './views/home.html',
  data: {
       meta: {
-        'title': 'Booknow - Flights - Hotels - Cars and Tours ',
-        'description': 'Book now is the best source for your online travel booking business. we offer last minute travel services such as flights hotels rooms tours and cars rental or transfer '
+        'title': 'Material CSS',
+        'description': ''
       }
     }
 }).
@@ -53,10 +53,7 @@ templateUrl: './views/partials/headdrop.html',
     };
 });
 
-
 app.controller('Home', ['$http','$scope',function ($http,$scope) {
-
-
 
 }]);
 
